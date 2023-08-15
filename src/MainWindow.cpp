520b6fc8d8c6bf9b27e4859344909c54cd00e270
Felipe Silva <felipemsvl@gmail.com>
2023-08-15 16:22:38 -0400

    Solution file for Visual Studio 2022 - change submodule url - opengl debug messages

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -4,17 +4,68 @@
 #include <graphics/Assets.h>
 #include <graph/SceneObject.h>
 #include <graph/SceneNode.h>
+#include <map>
 #include "BezierPatches.h"
 #include "Surface.h"
 
 namespace cg
 {
 
+static
+void debugCallback(GLenum source,
+    GLenum type,
+    GLuint id,
+    GLenum severity,
+    GLsizei length,
+    const GLchar* message,
+    const void* userParam)
+{
+    static std::map<GLuint, const char*> msg {
+        {GL_DEBUG_TYPE_ERROR,               "[  ERROR  ]"},
+        {GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR, "DEPRECATED_BEHAVIOR"},
+        {GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR,  "UNDEFINED_BEHAVIOR"},
+        {GL_DEBUG_TYPE_PORTABILITY,         "PORTABILITY"},
+        {GL_DEBUG_TYPE_PERFORMANCE,         "PERFORMANCE"},
+        {GL_DEBUG_TYPE_OTHER,               "OTHER"},
+        {GL_DEBUG_TYPE_MARKER,              "MARKER"},
+        {GL_DEBUG_TYPE_PUSH_GROUP,          "PUSH_GROUP"},
+        {GL_DEBUG_TYPE_POP_GROUP,           "POP_GROUP"},
+        {GL_DEBUG_SEVERITY_HIGH,            "HIGH"},
+        {GL_DEBUG_SEVERITY_MEDIUM,          "MEDIUM"},
+        {GL_DEBUG_SEVERITY_LOW,             "LOW"},
+        {GL_DEBUG_SEVERITY_NOTIFICATION,    "NOTIFICATION"},
+    };
+
+    if (severity == GL_DEBUG_SEVERITY_NOTIFICATION) return;
+    if (severity == GL_DEBUG_SEVERITY_LOW) return;
+    //if (severity == GL_DEBUG_SEVERITY_MEDIUM) return;
+    //if (severity == GL_DEBUG_SEVERITY_HIGH) return;
+
+    fprintf(stderr, "GL DEBUG (type=%s, severity=%s): %s",
+        msg[type], msg[severity], message);
+    fprintf(stderr, "\n");
+}
+
 void MainWindow::beginInitialize()
 {
+    const auto vendor   = glGetString(GL_VENDOR);
+    const auto renderer = glGetString(GL_RENDERER);
+    GLint version[2];
+
+    glGetIntegerv(GL_MAJOR_VERSION, version);
+    glGetIntegerv(GL_MINOR_VERSION, version+1);
+    
+    std::cerr << "CG Spline Project\n"
+        << "OpenGL version " << version[0] << "." << version[1] << "\n"
+        << "Renderer: " << renderer << "\n"
+        << "Vendor:   " << vendor << "\n\n";
+
     ImGui::GetIO().ConfigFlags |= ImGuiConfigFlags_DockingEnable;
 
     Assets::initialize();
+
+    glEnable(GL_DEBUG_OUTPUT);
+    glDebugMessageCallback(debugCallback, this);
 }
 
 void MainWindow::initializeScene()
@@ -42,8 +93,8 @@ void MainWindow::initializeScene()
         createSurfaceObject(*p, s);
     }
 
-    auto pipeline = new SurfacePipeline(0, *editor()->fragmentShader());
-    editor()->setPipeline(GLRenderer::PipelineCode::Surface, pipeline);
+    //auto pipeline = new SurfacePipeline(0, *editor()->fragmentShader());
+    //editor()->setPipeline(GLRenderer::PipelineCode::Surface, pipeline);
 }
 
 void MainWindow::render()
